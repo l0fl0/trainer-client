77f0e99599922bf387c2a4513ce1b9683122299a
Louis Flores <louisdefleurs@gmail.com>
2022-01-31 08:55:14 -0500

    client card component created

---
+++ client/src/components/ClientCard/ClientCard.jsx
@@ -0,0 +1,30 @@
+import "./ClientCard.scss";
+import tcLogo from "../../assets/images/tc-logo.png";
+
+export default function ClientCard({ name }) {
+	return (
+		<li className="client__card">
+			<div className="client__info">
+				<div className="client__info-profile">
+					<img src={tcLogo} alt="" className="client__info-image" />
+					<h2 className="client__info-title">{name}</h2>
+				</div>
+				<div>
+					<h3 className="client__info-subtitle">Next session</h3>
+					<p className="client__info-date">05:30pm, Friday, 02/04</p>
+				</div>
+			</div>
+			<ul className="client__icon-wrapper">
+				<li className="client__icon-border">
+					<i className="fas fa-comment client__icon"></i>
+				</li>
+				<li className="client__icon-border">
+					<i className="fas fa-phone client__icon"></i>
+				</li>
+				<li className="client__icon-border">
+					<i className="fas fa-dollar client__icon"></i>
+				</li>
+			</ul>
+		</li>
+	);
+}
